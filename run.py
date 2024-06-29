8164fbc51a9e95a421b57e68df2e9abfe9d073e6
Stephanie Bell <stephaniebellpalomino@hotmail.com>
2024-06-29 23:19:46 +0000

    Add welcome page function

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import time
+import os
+import sys
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -17,4 +20,61 @@ mar_expenses = SHEET.worksheet("March")
 
 jan_data = jan_expenses.get_all_values()
 
-print(jan_data)
+# Credit for clear screen function: https://www.geeksforgeeks.org/clear-screen-python/
+def clear_screen():
+    """
+    Clears CLI ahead of next code page
+    """
+    # for windows
+    if os.name == 'nt':
+        os.system('cls')
+ 
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        os.system('clear')
+
+#Python Typing Text Effect - www.101computing.net/python-typing-text-effect/
+def typingPrint(text):
+  for character in text:
+    sys.stdout.write(character)
+    sys.stdout.flush()
+    time.sleep(0.05)
+
+def welcome_page():
+    """
+    Displays welcome text
+    """
+    print(r'''
+    
+    $$$$$$$\                                                        $$\                     
+    $$  __$$\                                                       \__|                    
+    $$ |  $$ | $$$$$$\  $$$$$$$\  $$$$$$$\  $$\   $$\ $$\  $$\  $$\ $$\  $$$$$$$\  $$$$$$\  
+    $$$$$$$  |$$  __$$\ $$  __$$\ $$  __$$\ $$ |  $$ |$$ | $$ | $$ |$$ |$$  _____|$$  __$$\ 
+    $$  ____/ $$$$$$$$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$ | $$ | $$ |$$ |\$$$$$$\  $$$$$$$$ |
+    $$ |      $$   ____|$$ |  $$ |$$ |  $$ |$$ |  $$ |$$ | $$ | $$ |$$ | \____$$\ $$   ____|
+    $$ |      \$$$$$$$\ $$ |  $$ |$$ |  $$ |\$$$$$$$ |\$$$$$\$$$$  |$$ |$$$$$$$  |\$$$$$$$\ 
+    \__|       \_______|\__|  \__|\__|  \__| \____$$ | \_____\____/ \__|\_______/  \_______|
+                                            $$\   $$ |                                      
+                                            \$$$$$$  |                                      
+                                            \______/                                       
+
+    
+                                    Welcome back to Pennywise.
+
+Find out how much you have spent this month and if you've done enough to hold off Pennywise...
+
+    ''')
+    typingPrint("                                     Loading, please wait...")
+    time.sleep(12)
+    clear_screen()
+
+def main_menu():
+    """
+    
+    """
+
+def main():
+    welcome_page()
+    main_menu()
+
+main()
